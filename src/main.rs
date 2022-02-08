1c52146d89b5007544e6bbb6365add8cacbb440a
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2022-02-08 18:12:36 -0300

    Chapter 2.5 done

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ mod player;
 use player::*;
 mod rect;
 pub use rect::Rect;
+mod visibility_system;
+use visibility_system::VisibilitySystem;
 
 
 pub struct State {
@@ -21,8 +23,8 @@ impl GameState for State {
         read_input(self, ctx);
         self.run_systems();
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
+        let map = self.ecs.fetch::<Map>();
+        draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -35,6 +37,8 @@ impl GameState for State {
 
 impl State {
     fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem{};
+        vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -45,14 +49,14 @@ fn main() -> rltk::BError {
         .with_title("RogueLike Tutorial")
         .build()?;
     let mut gs = State { ecs: World::new() };
+    gs.ecs.register::<Viewshed>();
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
 
-    let (rooms, map) = new_map_rooms_and_corridors();
+    let map : Map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
     gs.ecs.insert(map);
-
-    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
         .create_entity()
@@ -63,6 +67,11 @@ fn main() -> rltk::BError {
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player {})
+        .with(Viewshed{
+            visible_tiles : Vec::new(),
+            range : 8,
+            dirty: true
+        })
         .build();
 
     rltk::main_loop(context, gs)
